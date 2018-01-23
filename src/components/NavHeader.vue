67ae081e05c2e7d5521e800b4adcfaf6ed5dc288
王晓北 <xiaobei.wang@fq960.com>
2018-01-23 18:07:03 +0800

    路由懒加载

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -32,7 +32,7 @@
             <a href="javascript:void(0)" class="navbar-link">Login</a>
             <div class="navbar-cart-container">
               <span class="navbar-cart-count">12</span>
-              <a class="navbar-link navbar-cart-link" href="/#/cart">
+              <a class="navbar-link navbar-cart-link" href="/cart">
                 <svg class="navbar-cart-logo">
                   <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-cart"></use>
                 </svg>
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,44 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 
-import GoodsList from '@/views/GoodsList';
-import Cart from '@/views/Cart';
+const _import = require('./_import_' + process.env.NODE_ENV)
 
 Vue.use(Router);
 
+export const asyncRouterMap = [
+  {
+    path: '*',
+    redirect: '/404',
+    hidden: true
+  }, {
+    path: '/error',
+    name: '401',
+    redirect: '/error/401',
+    hidden: true,
+    children: [
+      {
+        path: '401',
+        component: _import('errorPage/401')
+      }
+    ]
+  }, {
+    path: '/404',
+    name: '404',
+    component: _import('errorPage/404'),
+    hidden: true
+  }, {
+    path: '/',
+    component: _import('GoodsList'),
+    hidden: true
+  }, {
+    path: '/cart',
+    component: _import('Cart'),
+    hidden: true
+  }
+]
+
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'GoodsList',
-      component: GoodsList
-    },
-    {
-      path: '/cart',
-      name: 'Cart',
-      component: Cart
-    },
-  ]
-});
+  mode: 'history',
+  scrollBehavior: () => ({ y: 0 }),
+  routes: asyncRouterMap
+})
